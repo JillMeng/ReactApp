5e3337469320489de2dd99acdb771633510c75c3
JillMeng <76186275+JillMeng@users.noreply.github.com>
2022-03-09 22:19:23 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div class="App">
+
+        <div class="header aqua_backgound boader">
+            <h1>Jill Meng</h1>
+        </div>
+        <div class="footer aqua_backgound boader">
+            Footer
+        </div>
+        <div class="left aqua_backgound boader">
+            Left Side Bar
+        </div>
+        <div class="right aqua_backgound boader">
+            Right Side Bar
+        </div>
+        <div class="center aqua_backgound boader">
+            <div class="box hoverable"></div>
+        </div>
+
+    </div>
+  );
+}
+
+export default App;
